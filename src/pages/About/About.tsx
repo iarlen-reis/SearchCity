209772e5ae30612c1cf1c0d0f031e6d87c89a26d
= <iarlensr@hotmail.com>
2023-03-28 00:14:52 -0300

    feat: create page About, adding styles and animation.

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,12 +1,72 @@
 import React from "react";
-import { AboutContainerStyled } from "./styles";
+
+import { ContainerStyled } from "../../styles/Global";
+
+import AboutImage from "/about/AboutImage.png";
+
+import {
+  AboutContainer,
+  HeaderStyled,
+  UlStyled,
+  InfoDetailStyled,
+  ApiDetailStyled,
+  ImageContainer,
+} from "./styles";
 
 function About() {
   return (
-    <div>
-      <h1>Página About</h1>
-      <p>Eu sou a página de about</p>
-    </div>
+    <ContainerStyled>
+      <AboutContainer>
+        <section>
+          <HeaderStyled>
+            <h1>SearchCity</h1>
+            <p>
+              SearchCity é uma aplicação que utiliza as APIs do IBGE para
+              encontrar dados relacionados a uma cidade do Brasil.
+            </p>
+          </HeaderStyled>
+          <InfoDetailStyled>
+            <h2>Informações adicionais:</h2>
+            <p>
+              A aplicação foi construida com react utilizando typescript,
+              estilizada com styled-components e as requisições as APIs foram
+              feitas com Axios.
+            </p>
+            <p>
+              Para gerenciamento de estado foi utilizado o próprio contextAPI do
+              react eo roteamento foi feito com react-router-dom na versão 6.9.
+            </p>
+          </InfoDetailStyled>
+          <ApiDetailStyled>
+            <h2>APIs utilizadas:</h2>
+            <UlStyled>
+              <li>
+                <a
+                  href="https://servicodados.ibge.gov.br/api/docs/localidades"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  API dos Estados e Cidades
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="https://servicodados.ibge.gov.br/api/docs/malhas?versao=3#api-Malhas-municipiosIdGe"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  API das malhas (mapas)
+                </a>
+              </li>
+            </UlStyled>
+          </ApiDetailStyled>
+        </section>
+        <ImageContainer>
+          <img src={AboutImage} alt="" />
+        </ImageContainer>
+      </AboutContainer>
+    </ContainerStyled>
   );
 }
 
--- src/pages/About/styles.ts
+++ src/pages/About/styles.ts
@@ -1,10 +1,109 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-export const AboutContainerStyled = styled.div`
-  width: 100%;
-  background: ${({ theme }) => theme.colors.primary};
+const imageAnimation = keyframes`
+    0% {
+        top: 5%;
+    }
+    50% {
+        top: 20%;
+    }
+    100% {
+        top: 5%;
+    }
+`;
+
+export const AboutContainer = styled.main`
+  display: flex;
+`;
+
+export const HeaderStyled = styled.header`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  margin-top: 4rem;
+
+  color: ${({ theme }) => theme.colors.white};
 
   h1 {
+    font-size: 4rem;
+    font-family: "Itim", sans-serif;
+
+    color: ${({ theme }) => theme.colors.tertiary};
+  }
+
+  p {
+    max-width: 70rem;
+    font-size: 1.8rem;
+    font-weight: 500;
+  }
+`;
+
+export const InfoDetailStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 0.5rem;
+
+  margin-top: 3rem;
+
+  color: ${({ theme }) => theme.colors.white};
+
+  h2 {
+    font-size: 3.2rem;
+    font-family: "Itim", sans-serif;
+
+    color: ${({ theme }) => theme.colors.tertiary};
+  }
+
+  p {
+    max-width: 70rem;
+    font-size: 1.8rem;
+    font-weight: 500;
+  }
+`;
+
+export const ApiDetailStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  margin-top: 2rem;
+
+  h2 {
+    font-size: 3.2rem;
+    font-family: "Itim", sans-serif;
+
+    color: ${({ theme }) => theme.colors.tertiary};
+  }
+`;
+
+export const UlStyled = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  li a {
+    font-size: 2rem;
+
+    transition: 0.5s;
     color: ${({ theme }) => theme.colors.white};
+
+    &:hover {
+      color: rgba(255, 255, 255, 0.7);
+    }
+  }
+`;
+
+export const ImageContainer = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  width: 450px;
+
+  img {
+    position: absolute;
+    width: 100%;
+
+    animation: ${imageAnimation} 10s ease-in-out infinite;
   }
 `;
